1b23eefaa23e4be8443ec353761cf8fc15af23f0
Isha-Wagh <ishawagh17@gmail.com>
2025-07-23 08:21:32 +0530

    Added all files

---
+++ build/CMakeFiles/SmartWebCache.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SmartWebCache.dir/src/LRUCache.cpp.o"
+  "CMakeFiles/SmartWebCache.dir/src/LRUCache.cpp.o.d"
+  "CMakeFiles/SmartWebCache.dir/src/URLShortener.cpp.o"
+  "CMakeFiles/SmartWebCache.dir/src/URLShortener.cpp.o.d"
+  "CMakeFiles/SmartWebCache.dir/src/Validator.cpp.o"
+  "CMakeFiles/SmartWebCache.dir/src/Validator.cpp.o.d"
+  "CMakeFiles/SmartWebCache.dir/src/WebFetcher.cpp.o"
+  "CMakeFiles/SmartWebCache.dir/src/WebFetcher.cpp.o.d"
+  "CMakeFiles/SmartWebCache.dir/src/main.cpp.o"
+  "CMakeFiles/SmartWebCache.dir/src/main.cpp.o.d"
+  "SmartWebCache"
+  "SmartWebCache.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SmartWebCache.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
